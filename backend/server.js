f2de7a6905d299717694c5546b57b489bf095c52
Souvik De <souvik.de@sato-global.com>
2024-02-05 10:28:12 +0900

    Feat : Initial commit for the backend and the frontend of the MERN App

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const userRoutes = require("./routes/userRoutes")
+
+app.use(express.json())
+
+mongoose
+.connect(process.env.URI)
+.then(()=> {
+    console.log("Connected Successfully")
+    app.listen(process.env.PORT || 8000, (err)=>{
+        if (err) console.log(err)
+        console.log("Running Successfully at", process.env.PORT)
+    });
+})
+.catch((error)=>{
+    console.log("error",error)
+})
+
+app.use(userRoutes)
+
